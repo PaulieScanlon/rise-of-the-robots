a7be683da3239aae29308bfdeca1fc2f47544276
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-04-15 11:33:59 +0100

    chore: add additional image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,26 @@ require('dotenv').config({
 
 module.exports = {
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaults: {
+          quality: 90,
+          formats: ['auto', 'webp', 'avif'],
+          placeholder: 'blurred'
+        }
+      }
+    },
     {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST
       }
     }
   ]
