fb544e7ce46e9f8f7289fef32d32c47bded38d3a
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-04-22 15:57:41 -0400

    feat: add gatsby section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: ['button-gatsby-primary', 'button-gatsby-secondary'],
   theme: {
     fontFamily: {
       sans: ['Inconsolata-Regular', 'sans-serif'],
       heading: ['Inconsolata-Bold', 'sans-serif'],
+      bold: ['Inconsolata-Bold', 'sans-serif'],
       light: ['Inconsolata-ExtraLight', 'sans-serif'],
     },
     extend: {
       colors: {
         brand: {
           background: '#11081f',
-          primary: '#006ac1',
+          primary: '#2ca72c',
           secondary: '#7026b9',
           gatsby: '#663399',
           rive: '#d041ab',
           gray: '#b7b5bc',
+          link: '#a456f0',
         },
       },
       gridTemplateColumns: {
