62f9793304174f11a3526f349a97decbb493b6ab
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-05-03 15:08:02 -0700

    feat: refactor borders

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        downloadLocal: true,
+        downloadLocal: false,
         host: process.env.CONTENTFUL_HOST,
       },
     },
--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -77,12 +77,10 @@ const HeroSection = () => {
     border,
   } = contentfulHeroSection
 
-  console.log(border)
-
   return (
     <div className="grid lg:grid-cols-2 gap-12">
       <div className="grid gap-8">
-        <div className="grid gap-4 justify-items-center lg:justify-items-start">
+        <div className="grid gap-2 justify-items-center lg:justify-items-start">
           <img
             src={logo.svg.dataURI}
             alt={logo.title}
@@ -93,7 +91,7 @@ const HeroSection = () => {
             <h1 className="text-2xl md:text-3xl">{eventName}</h1>
             <h2 className="text-md font-light">{eventTagline}</h2>
           </div>
-          <div className="grid gap-4 text-center lg:text-left justify-items-center lg:justify-items-start">
+          <div className="grid gap-2 text-center lg:text-left justify-items-center lg:justify-items-start">
             <h3 className="text-md font-bold">{eventDetails}</h3>
             <a
               href={url}
