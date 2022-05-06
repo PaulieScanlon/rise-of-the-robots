51dba8923e762ce3fca1ae52a0f9f5679e63b9c2
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-05-06 10:05:24 -0700

    feat: resize images - add window.gtag - add redirect for partytown

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        downloadLocal: true,
+        downloadLocal: false,
         host: process.env.CONTENTFUL_HOST,
       },
     },
