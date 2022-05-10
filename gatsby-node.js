3c784698ac2b3a1161c1fe8c01e63345173dbd6a
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-05-10 11:23:44 -0700

    chore: cleanup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,3 @@
-const path = require('path')
-// const { copyLibFiles } = require('@builder.io/partytown/utils')
-
-exports.onPreBuild = async () =>
-  // { actions: { createRedirect } }
-  {
-    // await copyLibFiles(path.join(__dirname, 'static', '~partytown'))
-    // createRedirect({
-    //   fromPath: `~partytown/*`,
-    //   toPath: `https://riseoftherobots.gatsbyjs.io/~partytown/*`,
-    //   statusCode: 200,
-    // })
-  }
-
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     module: {
