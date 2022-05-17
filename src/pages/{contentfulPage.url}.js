6845f7cd1938d70fd23edd680bf871d74c1bc547
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-05-17 14:09:42 -0700

    feat: add webinar-section

--- src/pages/{contentfulPage.url}.js
+++ src/pages/{contentfulPage.url}.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
 
 import HeroSection from '../sections/hero-section'
+import WebinarSection from '../sections/webinar-section'
 import GatsbySection from '../sections/gatsby-section'
 import RiveSection from '../sections/rive-section'
 import FormSection from '../sections/form-section'
@@ -12,6 +13,9 @@ const getSection = (__typename) => {
   switch (__typename) {
     case 'ContentfulHeroSection':
       return <HeroSection />
+
+    case 'ContentfulWebinarSection':
+      return <WebinarSection />
 
     case 'ContentfulGatsbySection':
       return <GatsbySection />
@@ -35,14 +39,19 @@ const getSection = (__typename) => {
 
 const Page = ({
   data: {
-    contentfulPage: { sections },
+    contentfulPage: { sections, hiddenSections },
   },
 }) => {
   return (
     <Fragment>
       {sections.map((section, index) => {
         const { __typename } = section
-        return <section key={index}>{getSection(__typename)}</section>
+
+        return hiddenSections
+          .map((section) => section.content)
+          .includes(__typename) ? null : (
+          <section key={index}>{getSection(__typename)}</section>
+        )
       })}
     </Fragment>
   )
@@ -53,6 +62,9 @@ export default Page
 export const query = graphql`
   query ($id: String) {
     contentfulPage(id: { eq: $id }) {
+      hiddenSections {
+        content
+      }
       sections {
         __typename
       }
