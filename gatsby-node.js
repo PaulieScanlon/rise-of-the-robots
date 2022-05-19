ea3dde10569ba3cbf009f0151ba8885b37784cc2
tyhopp <hopp.ty.c@gmail.com>
2022-05-19 10:28:05 +0800

    chore: attempt redirects to partytown proxy

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,3 +10,19 @@ exports.onPostBuild = ({ store }) => {
   const { redirects } = store.getState()
   console.log(`Applied redirects:`, redirects)
 }
+
+exports.onCreatePages = ({ actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: `/~partytown/*`,
+    toPath: `/demos/rise-of-the-robots/~partytown/*`,
+    statusCode: 200,
+  })
+
+  createRedirect({
+    fromPath: `/__partytown-proxy?:url`,
+    toPath: `/demos/rise-of-the-robots/__partytown-proxy?:url`,
+    statusCode: 200,
+  })
+}
